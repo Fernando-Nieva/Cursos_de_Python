75e7459966f6d6ca79518f2ef4ab6a651db4f8ef
Fernando <nieva.cronos@gmail.com>
2024-10-18 17:53:45 -0300

    commit 0.0.4

---
+++ CodigoFacilito/paquetes/animales/__init__.py
@@ -0,0 +1,11 @@
+from .aves import Pinguino
+from .felinos import Jaguar
+
+
+
+def mi_funcion():
+    print("Mi funcion")
+    
+mi_jaguar= Jaguar()
+
+print("Este e sun mensaje del archivo init")
---
+++ CodigoFacilito/paquetes/principal.py
@@ -0,0 +1,31 @@
+# import calculadora
+
+# resultado = calculadora.suma(10,20)
+# print(resultado)
+
+# from calculadora import*
+# from calculadora import (suma,
+#                          resta,
+#                          division
+#                         )
+# from calculadora import suma as nueva_suma
+# from calculadora import resta 
+
+
+# resultado = nueva_suma(10,20)
+# print(resultado)
+# resultado2 = resta(10,20)
+# print(resultado2)
+
+# import calculadora
+# print(calculadora.__name__)
+# print(__name__)
+
+
+from animales import  Pinguino ,mi_jaguar,mi_funcion
+
+pinguino=Pinguino()
+pinguino.nadar()
+
+mi_jaguar.cazar()
+mi_funcion()
